136b98e67abc4f91369d87435d8a7f777a655d5d
MichaelWard405 <michaelgn405@gmail.com>
2026-06-19 18:35:18 +0000

    Syntax

--- Foolish_Invocation.sh
+++ Foolish_Invocation.sh
@@ -134,7 +134,7 @@ arch-chroot /mnt /bin/bash <<EOF
 set -e
 ln -sF /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
 hwclock --systohc
-sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8' /etc/locale.gen
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "Foolish" > /etc/hostname
